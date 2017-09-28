fe4913add0ea0ace97ae0523e0bc68934a7443a7
CumminUp07 <30929920+CumminUp07@users.noreply.github.com>
2017-09-28 22:49:31 +0000

    added method to scrape images from google
    This is a simple script to get an unlimited amount of images from google with any search terms. I will probably make changes to this in the future to make it more universal

---
+++ get_google_images.py
@@ -0,0 +1,53 @@
+
+# coding: utf-8
+
+# In[77]:
+
+
+from bs4 import BeautifulSoup as Soup
+import urllib2
+import json
+import urllib
+
+
+# In[78]:
+
+
+def get_links(query_string, num_images):
+    links = []
+    for i in range(0,num_images,100):
+        url = 'https://www.google.com/search?ei=1m7NWePfFYaGmQG51q7IBg&hl=en&q='+query_string+'&tbm=isch&ved=0ahUKEwjjovnD7sjWAhUGQyYKHTmrC2kQuT0I7gEoAQ&start='+str(i)+'&yv=2&vet=10ahUKEwjjovnD7sjWAhUGQyYKHTmrC2kQuT0I7gEoAQ.1m7NWePfFYaGmQG51q7IBg.i&ijn=1&asearch=ichunk&async=_id:rg_s,_pms:s'
+        request = urllib2.Request(url, None, {'User-Agent': 'Mozilla/5.0'})
+        json_string = urllib2.urlopen(request).read()
+        page = json.loads(json_string)
+        html = page[1][1]
+        new_soup = Soup(html,'html')
+        imgs = new_soup.find_all('img')
+        for j in range(len(imgs)):
+            links.append(imgs[j]["src"])
+    return links
+
+
+# In[79]:
+
+
+def get_images(links,pre):
+    for i in range(len(links)):
+        urllib.urlretrieve(links[i], "./images/"+str(pre)+str(i)+".jpg")
+        
+
+
+# In[ ]:
+
+
+terms = ["cars","numbers","scenery","people","dogs","cats","animals"]
+for x in range(len(terms)):
+    all_links = get_links('animated+'+terms[x],1000)
+    get_images(all_links,x)
+
+
+# In[ ]:
+
+
+
+
